e67afb609dd5efb47d373c5cc8781d50ab9ae912
TimY90 <124255000+TimY90@users.noreply.github.com>
2025-05-06 17:33:22 +0100

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -17,7 +17,7 @@ app.use('/api/mood', moodRoutes);
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/wellnessdb', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
