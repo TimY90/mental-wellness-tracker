46d2712b399d1e694b7f5e064a43a442925a6e9a
Timothy Young <yout2_21@uni.worc.ac.uk>
2025-05-07 21:52:13 +0100

    Show Navbar on all pages

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,14 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import MoodForm from './pages/MoodForm';
 import MoodHistory from './pages/MoodHistory';
-import Navbar from './components/Navbar';
-import './App.css';
 import MoodChart from './components/MoodChart';
+import Navbar from './components/Navbar'; // ✅ Used now
+import './App.css';
 
 function App() {
   return (
     <Router>
+      {/* ✅ Navbar now visible on all pages */}
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
