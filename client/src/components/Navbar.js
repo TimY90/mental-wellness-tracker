62534361995f21dc5c3b0dc962ceca70ae3f3921
Timothy Young <yout2_21@uni.worc.ac.uk>
2025-05-08 03:53:04 +0100

    Fix CSS import path in Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './App.css'; // Import the CSS (can be replaced with './Navbar.css' if modularized)
+import '../App.css'; // Import the CSS (can be replaced with './Navbar.css' if modularized)
 
 function Navbar() {
   const navigate = useNavigate();
